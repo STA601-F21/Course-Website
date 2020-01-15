a82643adbcb0fefaadb6d086acd597e31b6dafae
Olanrewaju Akande <akandelanre13@gmail.com>
2020-01-15 16:16:10 -0500

    Add PDF versions to the website

---
+++ static/slides/lec-slides/CovertToPdf.R
@@ -0,0 +1,8 @@
+
+
+
+library(webshot)
+webshot("/Users/oma9/Google Drive/GitHub/Courses/2020/Spring/STA 602L/Course-Website/static/slides/lec-slides/01-intro.html",
+        "01-intro.pdf",delay=10)
+webshot("/Users/oma9/Google Drive/GitHub/Courses/2020/Spring/STA 602L/Course-Website/static/slides/lec-slides/02-one-parameter-models-I.html",
+        "02-one-parameter-models-I.pdf",delay=10)
