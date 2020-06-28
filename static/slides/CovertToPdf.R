2ed1076fb88c23fa68fc4ecfa12ea6f44b627dc6
Olanrewaju Akande <akandelanre13@gmail.com>
2020-06-28 12:09:34 -0400

    Add pdf versions

--- static/slides/CovertToPdf.R
+++ static/slides/CovertToPdf.R
@@ -1,10 +1,17 @@
 
 
+#remotes::install_github("rstudio/webshot2")
+#library(webshot2)
 
 library(webshot)
 #install_phantomjs(force=T)
-webshot("/Users/oma9/Google Drive/GitHub/Courses/2020/Spring/STA 602L/Course-Website/static/slides/lec-slides/01-intro.html",
-        "01-intro.pdf",delay=10)
+
+
+webshot("/Users/oma9/Google Drive/GitHub/Courses/2020/Summer/STA 360:602L/Course-Website/static/slides/01-course-overview.html",
+        "01-course-overview.pdf",delay=10)
+
+rmdshot("/Users/oma9/Google Drive/GitHub/Courses/2020/Summer/STA 360:602L/Course-Website/static/slides/01-course-overview.rmd",
+        "01-course-overview.pdf",delay=10)
 
 webshot("https://sta-602l-s20.github.io/Course-Website/slides/lec-slides/02-one-parameter-models-I.html",
         "02-one-parameter-models-I.pdf",delay=10)
@@ -14,3 +21,5 @@ webshot("/Users/oma9/Google Drive/GitHub/Courses/2020/Spring/STA 602L/Course-Web
 
 #pagedown::chrome_print("/Users/oma9/Google Drive/GitHub/Courses/2020/Spring/STA 602L/Course-Website/static/slides/lec-slides/02-one-parameter-models-I.html",
 #                       wait=10)
+
+
